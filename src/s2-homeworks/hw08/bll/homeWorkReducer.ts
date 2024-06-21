60433e28533bafc4e308b5e8bb46278370b55ebe
Anna <blinova.anneta@gmail.com>
2024-06-21 13:25:26 +0200

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,25 @@
 import {UserType} from '../HW8'
 
+const initialPeople: Array<UserType> = []
+
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: Array<UserType> = initialPeople, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const sortedItems = action.payload === 'up' ? 1 : -1;
+            return [...state].sort((a, b) => 
+                (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1) * sortedItems
+            )
         }
         case 'check': {
-
-            return state // need to fix
+            const currentItems = state.filter(el => el.age >= action.payload)
+            return currentItems // need to fix
         }
         default:
             return state
     }
 }
+
