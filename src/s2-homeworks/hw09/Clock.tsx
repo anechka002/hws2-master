3cef9218da6df4771ec0b9f1138bfde4fa56d65d
Anna <blinova.anneta@gmail.com>
2024-10-28 12:12:13 +0100

    commit2

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -8,7 +8,6 @@ function Clock() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
     const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
     const [show, setShow] = useState<boolean>(false)
-    console.log(timerId)
 
     useEffect(() => {
         if(timerId) {
@@ -47,11 +46,16 @@ function Clock() {
     }
 
     const stringTime = date.toLocaleTimeString() // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString() // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    
+    const stringDate = date.toLocaleDateString('ru-Ru') // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = date.toLocaleString('en-US', {weekday: 'long'})// пишут студенты
     const stringMonth = date.toLocaleString('en-US', {month: 'long'}) // пишут студенты
+    // console.log(stringTime)
+    // console.log(stringDate)
+    // console.log(stringDay)
+    // console.log(stringMonth)
 
     return (
         <div className={s.clock}>
